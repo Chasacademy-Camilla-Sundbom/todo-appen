e4e8090bbd4ca3d9908e3c0a8bc97b73e741cc10
Camilla Sundbom <camilla.sundbom@outlook.com>
2023-06-13 09:35:12 +0200

    first app crashed so made a new one

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Add from "./screens/Add";
+import Detail from "./screens/Detail";
+import Home from "./screens/Home";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Group>
+          <Stack.Screen
+            name="Todo"
+            component={Home}
+            options={({ navigation, route }) => ({
+              headerRight: () => <Button title="Add" />,
+            })}
+          />
+          <Stack.Screen name="Details" component={Detail} />
+        </Stack.Group>
+        <Stack.Group screenOptions={{ presentation: "modal" }}>
+          <Stack.Screen name="New Todo" component={Add} />
+        </Stack.Group>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
